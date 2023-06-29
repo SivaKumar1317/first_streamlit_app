5a9a03e22e13ec5d3efcf2b1dab0927232116b92
SivaKumar1317 <138103502+SivaKumar1317@users.noreply.github.com>
2023-06-29 22:53:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,4 @@ streamlit.dataframe(fruityvice_normalized)
 streamlit.header('Fruitvice Fruit Advie')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+import request
